8f4a8806237876a972238e029a329899d4c2c429
Eugen Alekseev <devilsbrother1@gmail.com>
2025-02-08 18:19:29 +0200

    fix versel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+const base =
+  process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
+    ? '/goit-react-hw-01/'
+    : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   build: {
     sourcemap: true,
