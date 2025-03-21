fcde668d7f3e351afc5f17e8c699d5c4ba1fddb9
Eugen Alekseev <devilsbrother1@gmail.com>
2025-03-21 12:45:39 +0200

    .

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
-import css from './FriendListItem.module.css';
-import { FriendListItem } from './FriendListItem';
+import css from './FriendList.module.css';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
   return (
